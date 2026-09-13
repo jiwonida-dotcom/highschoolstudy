3fabd1d28eecf5fcaa25bf07999e54608d786400
jiwonida-dotcom <jiwonida@gmail.com>
2026-09-13 15:58:19 +0900

    Update docs

--- push.bat
+++ push.bat
@@ -1,38 +1,20 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-set LOG=push-log.txt
-break > "%LOG%"
 
-call :run git --version
-call :run git config --get user.name
-call :run git config --get user.email
+echo === highschoolstudy push ===
+echo.
 
-if not exist ".git" call :run git init
-call :run git remote remove origin
-call :run git remote add origin https://github.com/jiwonida-dotcom/highschoolstudy.git
-call :run git add -A
-call :run git commit -m "Add Unyang High School admissions coach project docs"
-call :run git branch -M main
-call :run git push -u origin main
-call :run git log --oneline
-call :run git status --short --branch
+git remote add origin https://github.com/jiwonida-dotcom/highschoolstudy.git 2>nul
+git remote set-url origin https://github.com/jiwonida-dotcom/highschoolstudy.git
+git add -A
+git commit -m "Update docs" 2>nul
+git branch -M main
+git push -u origin main
 
 echo.
-echo ==========================================
-echo 로그 파일: %CD%\%LOG%
+git log --oneline
+git status -sb
+echo.
 echo 확인: https://github.com/jiwonida-dotcom/highschoolstudy
-echo ==========================================
-echo.
 pause
-goto :eof
-
-:run
-echo.>> "%LOG%"
-echo ### %*>> "%LOG%"
-echo.
-echo ^> %*
-%* >> "%LOG%" 2>&1
-echo   exit=%ERRORLEVEL%
-echo [exit=%ERRORLEVEL%]>> "%LOG%"
-goto :eof
